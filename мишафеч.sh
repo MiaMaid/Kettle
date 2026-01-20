bf9a4ae330d83a52a7cc919d4bf62e68ae51a371
rizl123 <rizl.sanding@proton.me>
2026-01-20 21:05:22 +0500

    кэш
    Проверка зависимостей
    Кэширование изображения

--- мишафеч.sh
+++ мишафеч.sh
@@ -1,10 +1,28 @@
-cd $HOME && git clone https://github.com/dylanaraps/neofetch
-cd neofetch
-sudo make install
-cd
-wget -P $HOME https://i.postimg.cc/859J983B/Mixa-P52fetch.png
-neofetch --kitty Mixa-P52fetch.png
-rm $HOME/Mixa-P52fetch.png
+#!/bin/bash
+
+DEPENDENCIES=(git wget)
+
+for tool in "${DEPENDENCIES[@]}"; do
+    if ! command -v "$tool" &> /dev/null; then
+        echo "Ой... кажется, у тебя не установлен '$tool'.. :( Пожалуйста, поставь его, чтобы все заработало!"
+        exit 1
+    fi
+done
+
+CACHE_IMG="$HOME/.cache/Mixa-P52fetch.png"
+
+if ! command -v neofetch &> /dev/null; then
+    git clone https://github.com/dylanaraps/neofetch /tmp/neofetch
+    cd /tmp/neofetch && sudo make install
+    cd ~ && rm -rf /tmp/neofetch
+fi
+
+if [ ! -f "$CACHE_IMG" ]; then
+    mkdir -p "$(dirname "$CACHE_IMG")"
+    wget -q -O "$CACHE_IMG" https://i.postimg.cc/859J983B/Mixa-P52fetch.png
+fi
+
+neofetch --kitty "$CACHE_IMG"
 
 ##############################################
 ### Кто читает скрипт, тот лох. Кроме Миши ###
