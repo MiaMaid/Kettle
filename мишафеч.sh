21d4ac64963d4a95a79fd9e11eee5609dbb7f794
Mia <miamaid777@gmail.com>
2026-01-20 21:49:04 +0700

    Add files via upload

---
+++ мишафеч.sh
@@ -0,0 +1,11 @@
+cd $HOME && git clone https://github.com/dylanaraps/neofetch
+cd neofetch
+sudo make install
+cd
+wget -P $HOME https://i.postimg.cc/859J983B/Mixa-P52fetch.png
+neofetch --kitty Mixa-P52fetch.png
+rm $HOME/Mixa-P52fetch.png
+
+##############################################
+### Кто читает скрипт, тот лох. Кроме Миши ###
+##############################################
